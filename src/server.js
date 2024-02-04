bdd8746673606f957a6cf615c71cfe51ac87ae8e
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2024-02-05 01:15:44 +0700

    update-init

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 import express from 'express';
+
+//
 import { mapOrder } from '~/utils/sorts.js';
 
 const app = express();
 const hostname = 'localhost';
-const port = 8017;
+const port = 3000;
 
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
@@ -31,5 +27,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
+  console.log(`Trello-api running at http://${hostname}:${port}/`);
 });

f40bce5b09d149280a2c72af717d67fd8f906106
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2024-04-01 00:11:04 +0700

    config-domain-production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,15 @@
 // Các domain cho phép được sử dụng tài nguyên từ server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
+  // Domain Developer
+  // 'http://localhost:5173',
+
   // Sau này các domain deploy ghi vào đây
+  'http://trello-web-psi.vercel.app'
 ];
 
 //
 export const BOARD_TYPES = {
   PUBLIC: 'public',
   PRIVATE: 'private',
-  WORKSPACE: 'workspace'
+  WORKSPACE: 'workspace',
 };
